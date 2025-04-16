663fda569637a7a78222f55976ef183a1a86ab3e
sneghasenthil <sneghasenthil@gmail.com>
2025-04-16 12:25:10 +0530

    feat: added query and explanation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
+import time
 
 GOOGLE_API_KEY = "AIzaSyCuYZ4RlnhFhwmWqrFSts1A6R7TGb1DRTQ"
 
@@ -23,11 +24,63 @@ def main():
     submit=st.button("generate")
 
     if submit:
-        
-        response = model.generate_content(text_input)
+        with st.spinner("Generate SQL Query..."):
+            template ="""
+                create a SQL query snippet using below text:
 
-        print(response.text)
-        st.write(response.text)
+                ```
+                        {text_input}
+                ```
+                I just want a SQL Query
+                """
+            
+            formatted_template = template.format(text_input=text_input)
+
+            
+            response=model.generate_content(formatted_template)
+
+            sql_query = response.text
+            sql_query=sql_query.strip().lstrip("```sql").rstrip("```")
+
+            expected_output ="""
+                What would be the expected response of this SQL query snippet:
+
+                ```
+                        {sql_query}
+                ```
+                Provide sample tabalur response with no explanation
+
+                """
+            expected_output_formatted=expected_output.format(sql_query=sql_query)
+            e_output=model.generate_content(expected_output_formatted)
+            e_output=e_output.text
+            
+
+            explanation="""
+                Explain this SQL query :
+
+                ```
+                        {sql_query}
+                ```
+                Provide simple explanation
+
+                """
+            
+            explanation_formatted=explanation.format(sql_query=sql_query)
+            explanation=model.generate_content(explanation_formatted)
+            explanation=explanation.text
+
+            with st.container():
+                st.success("SQL query generated Successfully")
+                st.code(sql_query,language="sql")
+
+                time.sleep(5)
+
+                st.success("Explanation for this SQL Query")
+                st.markdown(explanation)
+
+
+
 
 
 
