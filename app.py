243d376f071b5a1f685503d9eb58619bc2ff9b27
sneghasenthil <sneghasenthil@gmail.com>
2025-04-16 09:57:18 +0530

    feat : streamlit used

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("this is sample demo")
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="AI Assist SQL Generator", page_icon=":robot:")
+    st.header("AI Assist SQL Generator")
+
+main()
+
