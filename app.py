cd29a0b04ce4b68fa5f7316572fd0e301b73ddbf
snegha-29 <sneghasenthil@gmail.com>
2025-04-16 10:23:57 +0530

    feat: textarea and button added

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="AI Assist SQL Generator", page_icon=":robot:")
     st.header("AI Assist SQL Generator")
+    st.markdown(""" 
+    <div>
+    <h3>I can generate SQL Query for You!!!</h3> 
+    </div>
+    """, unsafe_allow_html=True,
+    )
 
+text_input=st.text_area ("Enter Your Need Here ")
+submit=st.button("generate")
 main()
 
